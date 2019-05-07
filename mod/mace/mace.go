3795ab5c591ab07414d1c923133d09238db5b64e
Brandur <brandur@brandur.org>
2019-05-07 07:22:47 -0700

    Add mess to try and tease undesired functionality out of Ace rendering

--- mod/mace/mace.go
+++ mod/mace/mace.go
@@ -4,19 +4,61 @@ import (
 	"bufio"
 	"html/template"
 	"os"
+	"strings"
 
 	"github.com/brandur/modulr/context"
 	"github.com/pkg/errors"
 	"github.com/yosssi/ace"
 )
 
-func Load(c *context.Context, basePath, innerPath string, opts *ace.Options) (*template.Template, bool, error) {
+func Load(c *context.Context, basePath, innerPath string, opts *ace.Options) (*template.Template, error) {
 	if opts == nil {
 		opts = &ace.Options{}
 	}
 
-	aceBasePath := basePath + ".ace"
-	aceInnerPath := innerPath + ".ace"
+	// Ace made a really strange decision to not take extensions when passing
+	// around the names of templates, which makes working with known files
+	// unnecessarily difficult. Here we correct that by allowing an extension
+	// to be passed to the function and stripping it off for Ace's benefit.
+ 	extlessBasePath := basePath
+	if strings.HasSuffix(basePath, ".ace") {
+		extlessBasePath = strings.TrimSuffix(basePath, ".ace")
+	}
+	extlessInnerPath := innerPath
+	if strings.HasSuffix(innerPath, ".ace") {
+		extlessInnerPath = strings.TrimSuffix(innerPath, ".ace")
+	}
+
+	// See the comment above for some context, but since Ace caches templates
+	// we always invoke Load and depend on it to take its own fast path if we
+	// didn't set DynamicReload.
+	template, err := ace.Load(extlessBasePath, extlessInnerPath, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	c.Log.Debugf("mace: Loaded template layout '%s' view '%s'",
+		basePath, innerPath)
+
+	return template, nil
+}
+
+// Load2 loads a template.
+//
+// TODO: Get rid of this.
+func Load2(c *context.Context, basePath, innerPath string, opts *ace.Options) (*template.Template, bool, error) {
+	if opts == nil {
+		opts = &ace.Options{}
+	}
+
+	aceBasePath := basePath
+	if !strings.HasSuffix(basePath, ".ace") {
+		aceBasePath = basePath + ".ace"
+	}
+	aceInnerPath := innerPath
+	if !strings.HasSuffix(innerPath, ".ace") {
+		aceInnerPath = innerPath + ".ace"
+	}
 
 	// TODO: Check existence of files.
 
@@ -32,27 +74,68 @@ func Load(c *context.Context, basePath, innerPath string, opts *ace.Options) (*t
 		opts.DynamicReload = true
 	}
 
+ 	extlessBasePath := basePath
+	if strings.HasSuffix(basePath, ".ace") {
+		extlessBasePath = strings.TrimSuffix(basePath, ".ace")
+	}
+	extlessInnerPath := innerPath
+	if strings.HasSuffix(innerPath, ".ace") {
+		extlessInnerPath = strings.TrimSuffix(innerPath, ".ace")
+	}
+
 	c.Log.Debugf("mace: Loaded template layout '%s' view '%s'",
 		basePath, innerPath)
 
 	// See the comment above for some context, but since Ace caches templates
 	// we always invoke Load and depend on it to take its own fast path if we
 	// didn't set DynamicReload.
-	template, err := ace.Load(basePath, innerPath, opts)
+	template, err := ace.Load(extlessBasePath, extlessInnerPath, opts)
 
 	return template, changedBasePath || changedInnerPath, err
 }
 
 // Render is a shortcut for loading an Ace template and rendering it to a
 // target file.
+func Render(c *context.Context, basePath, innerPath, target string,
+	opts *ace.Options, locals map[string]interface{}) error {
+
+	template, err := Load(c, basePath, innerPath, opts)
+	if err != nil {
+		return errors.Wrap(err, "Error loading template")
+	}
+
+	file, err := os.Create(target)
+	if err != nil {
+		return errors.Wrap(err, "Error creating target file")
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	err = template.Execute(writer, locals)
+	if err != nil {
+		return errors.Wrap(err, "Error rendering template")
+	}
+
+	c.Log.Debugf("mace: Rendered view '%s' to '%s'",
+		innerPath, target)
+
+	return nil
+}
+
+// Render2 is a shortcut for loading an Ace template and rendering it to a
+// target file.
 //
 // Note that the values in locals are not considered when determining whether
 // the sources are unchanged, so make sure to use a forced context in case a
 // change is made to them.
-func Render(c *context.Context, basePath, innerPath, target string,
+//
+// TODO: Get rid of this.
+func Render2(c *context.Context, basePath, innerPath, target string,
 	opts *ace.Options, locals map[string]interface{}) (bool, error) {
 
-	template, changed, err := Load(c, basePath, innerPath, opts)
+	template, changed, err := Load2(c, basePath, innerPath, opts)
 	if err != nil {
 		return changed, errors.Wrap(err, "Error loading template")
 	}
